3bbace20e20323aa3a7302199f86ce1c239d918c
79602 <vishivtonya@gmail.com>
2023-06-11 13:57:50 +0300

    Initial Commit

---
+++ lab5.py
@@ -0,0 +1,97 @@
+# b = min(1, 2, 4, 0, -5, -10)
+# print(b)
+
+# b = max(1, 2, 3, 0, -5, -10)
+# print(b)
+
+# b = pow(6, 2)
+# print(b)
+
+# b = pow(49, 0.5)
+# print(b)
+
+# b = pow(27, 1/3)
+# print(b)
+
+# b = round(0.5)
+# print(b)
+
+# b = round(1.5)
+# print(b)
+
+# b = round(10.50000001)
+# print(b)
+
+# b = round(7.8756, 2)
+# print(b)
+
+# b = round(7.8756, 3)
+# print(b)
+
+# b = round(7.8756, -1)
+# print(b)
+
+# b = round(784.5575, -2)
+# print(b)
+
+# b = round(784.5575, -1)
+# print(b)
+
+# b = max(1, 2, abs(-3), -10)
+# print(b)
+
+# b = max(1, 2, abs(min(10, 5, -3)), -10)
+# print(b)
+# import math
+# b = math.ceil(5.2)
+# print(b)
+
+# import math
+# b = math.ceil(-5.2)
+# print(b)
+
+# import math
+# b = math.floor(5.99)
+# print(b)
+
+# import math
+# b = math.floor(-3.3)
+# print(b)
+
+# import math
+# b = math.factorial(6)
+# print(b)
+
+# import math
+# b = math.trunc(5.99)
+# print(b)
+
+# b = int(5.8)
+# print(b)
+# import math
+#
+# b = math.log2(4)
+# print(b)
+#
+# b = math.log10(1000)
+# print(b)
+#
+# b = math.log(2.7)
+# print(b)
+#
+# b = math.pi
+# print(b)
+#
+# b = math.e
+# print(b)
+
+# b = math.sqrt(49)
+# print(b)
+
+
+
+
+
+
+
+
---
+++ type_id.py
@@ -0,0 +1,23 @@
+# a = 7
+# a = 6.8
+# a = 4
+# print(a + a)
+# print(a * a)
+
+# a, b = 1, 2
+# print(a, b)
+# a, b = b, a
+# print(a,b)
+# print(id(a), id(b))
+# print(type(a))
+
+x = 5.8
+s = 'Hello'
+
+print(type(x), type(s))
+print(s)
+b = 'True'
+print(b)
+
+type = 7
+print(type)
